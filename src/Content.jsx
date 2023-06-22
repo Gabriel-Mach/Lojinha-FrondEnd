86f7a0b09c5b511f31933ba30bbf92fa960b414c
Gabriel-Mach <59069859+Gabriel-Mach@users.noreply.github.com>
2023-06-22 13:59:49 -0300

    Update Content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route} from "react-router-dom"
 import { Store } from "./pages/Store";
 import {Cart} from "./pages/Cart"
 
